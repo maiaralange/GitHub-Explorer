1beb27ae517526b90d4c2911e87c0887c4a24c4c
Maiara Lange <maiaralange@microsoft.com>
2022-07-27 18:20:41 -0300

    Including access to github API

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { RepositoryItem } from "./RepositoryItem";
+
+import "../styles/repositories.scss";
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Repository List</h1>
+      <ul>
+        {repositories.map((repository, index) => (
+          <RepositoryItem key={index} repository={repository} />
+        ))}
+      </ul>
+    </section>
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
-const environmentConfig = {
-  "development": {
-    mode: "development",
-    devtool: "eval-source-map"
-  },
-  "production": {
-    mode: "production",
-    devtool: "source-map"
-  }
-}; 
+const isDevelopment = process.env.NODE_ENV === "development";
 
 module.exports = {
-  ...environmentConfig[process.env.NODE_ENV],
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -23,19 +16,33 @@ module.exports = {
     extensions: [".js", ".jsx"]
   },
   devServer: {
-    static: path.resolve(__dirname, "public")
+    static: path.resolve(__dirname, "public"),
+    hot: true
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html")
-    })
-  ],
+    }),
+    isDevelopment && new ReactRefreshWebpackPlugin()
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: "babel-loader"
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel")
+            ].filter(Boolean)
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   }
