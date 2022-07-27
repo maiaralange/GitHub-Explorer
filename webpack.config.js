ed7fbf95fb68214669ab23e323a8b0cd82326649
Maiara Lange <maiaralange@microsoft.com>
2022-07-27 11:34:52 -0300

    Configuration changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const environmentConfig = {
+  "development": {
+    mode: "development",
+    devtool: "eval-source-map"
+  },
+  "production": {
+    mode: "production",
+    devtool: "source-map"
+  }
+}; 
 
 module.exports = {
-  mode: "development",
+  ...environmentConfig[process.env.NODE_ENV],
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -10,6 +22,14 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"]
   },
+  devServer: {
+    static: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html")
+    })
+  ],
   module: {
     rules: [
       {
